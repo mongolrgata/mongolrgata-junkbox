4f3ade79a7df1836beadfb83d35bf4032ccd5ed2
mongolrgata <mongolrgata@gmail.com>
2015-07-14 21:03:43 +0300

    comments

--- trans-ws2json/script.js
+++ trans-ws2json/script.js
@@ -14,14 +14,15 @@ var $template = $(
     '             <div class="name">                                                             ' +
     '             </div>                                                                         ' +
     '             <div class="line-state">                                                       ' +
-    '                   <label><input class="null" type="radio" value="0">TODO</label><br>       ' +
+    '                   <label><input class="null" type="radio" value="0">Сделай!</label><br>    ' +
     '                   <label><input class="init" type="radio" value="1">Черновик</label><br>   ' +
-    '                   <label><input class="cont" type="radio" value="2">На контроле</label><br>' +
+    '                   <label><input class="cont" type="radio" value="2">Вычитка</label><br>    ' +
     '                   <label><input class="best" type="radio" value="3">Итоговый</label><br>   ' +
     '             </div>                                                                         ' +
     '             <button class="quote">quote</button>                                           ' +
     '         </div>                                                                             ' +
     '      </div>                                                                                ' +
+    '      <button class="add-comm"> </button>                                                   ' +
     '   </td>                                                                                    ' +
     '   <td>                                                                                     ' +
     '      <div class="lines">                                                                   ' +
@@ -79,6 +80,7 @@ function parseFileData() {
         var st = f_data[i].line.state || 0;
         var en = f_data[i].line.en.replace(/\\n/g, '<br>');
         var ru = f_data[i].line.ru || '';
+        var comms = f_data[i].line.comments || [];
         var name = f_data[i].name;
 
         function recount() {
@@ -105,7 +107,7 @@ function parseFileData() {
             }
         }
 
-        var foo = function () {
+        var changeState = function () {
             var $radio = $(this);
             var $row = $radio.closest('.line-row');
             var oldData = $row.data('linkedObj');
@@ -119,24 +121,51 @@ function parseFileData() {
             recount();
         };
 
+        var addComm = function (val) {
+            var $commBox = $('<div class="comm-box"/>').append(
+                $('<input type="text" class="comm">').val(val instanceof Object ? '' : val).change(function () {
+                    if ($('#silent-save').prop('checked')) {
+                        silentSave();
+                    }
+                })
+            );
+
+            $commBox.append(
+                $('<button class="del-comm"/>').text('удлт').click((function () {
+                    this.detach();
+
+                    if ($('#silent-save').prop('checked')) {
+                        silentSave();
+                    }
+                }).bind($commBox))
+            );
+
+            this.find('.lines').append($commBox);
+        };
+
         $temp.find('.en-line').html(en);
         $temp.find('.name').text(name || '<NONAME>').prop('title', name);
         $temp.find('.id-code').text('ID:' + id);
         $temp.find('.line-state');
         $temp.find('label input').prop('name', id);
-        $temp.find('.null').prop('checked', st == 0).change(foo);
-        var $init = $temp.find('.init').prop('checked', st == 1).change(foo);
+        $temp.find('.null').prop('checked', st == 0).change(changeState);
+        var $init = $temp.find('.init').prop('checked', st == 1).change(changeState);
         var $text = $temp.find('.ru-line').find('textarea').val(ru).change((function () {
             this.prop('checked', true);
-            foo.call(this);
+            changeState.call(this);
         }).bind($init));
-        $temp.find('.cont').prop('checked', st == 2).change(foo);
-        $temp.find('.best').prop('checked', st == 3).change(foo);
+        $temp.find('.cont').prop('checked', st == 2).change(changeState);
+        $temp.find('.best').prop('checked', st == 3).change(changeState);
         $temp.find('.quote').click((function () {
             this.val('«' + this.val() + '»');
             this.focus();
             this[0].setSelectionRange(this.val().length - 1, this.val().length - 1);
         }).bind($text));
+        $temp.find('.add-comm').click(addComm.bind($temp));
+
+        for (var j = 0, len = comms.length; j < len; ++j) {
+            addComm.call($temp, comms[j]);
+        }
 
         $linesBox.append($temp.data('linkedObj', f_data[i]));
 
@@ -164,31 +193,44 @@ function colorScroll() {
             position: 'absolute',
             left: 0,
             right: 0,
-            top: ((100/n)*i) + '%',
-            bottom: ((100/n)*(n-i-1)) + '%'
+            top: ((100 / n) * i) + '%',
+            bottom: ((100 / n) * (n - i - 1)) + '%'
         });
 
         $div.appendTo($cs);
     }
 }
 
-function silentSave() {
+function bigSave(doMyThing) {
     var rows = $('.line-row');
     var count = rows.length;
     var newData = [];
 
-    function doMyThing() {
-        setData(newData);
-    }
-
     rows.each(function (index, element) {
         var line = $(element);
         var rec = line.data('linkedObj');
+        var comms = line.find('.comm');
 
         rec.line.ru = line.find('textarea').val();
+        if (comms.length > 0) {
+            rec.line.comments = [];
+
+            for (var i = 0, n = comms.length; i < n; ++i) {
+                rec.line.comments.push($(comms[i]).val());
+            }
+        }
+        else {
+            delete rec.line.comments;
+        }
         newData.push(rec);
 
-        if (!--count) doMyThing();
+        if (!--count) doMyThing(newData);
+    });
+}
+
+function silentSave() {
+    bigSave(function doMyThing(newData) {
+        setData(newData);
     });
 }
 
@@ -210,37 +252,19 @@ $(document).ready(function () {
         reader.readAsText(f_in);
     });
 
-    $('#silent-save').prop('checked', getSaveMode() === 'silent').change(function() {
+    $('#silent-save').prop('checked', getSaveMode() === 'silent').change(function () {
         setSaveMode($(this).prop('checked') ? 'silent' : 'manual');
     });
 
     $('#just-save').click(function () {
-        var rows = $('.line-row');
-        var count = rows.length;
-        var newData = [];
-
-        function doMyThing() {
+        bigSave(function doMyThing(newData) {
             setData(newData);
             alert('saved');
-        }
-
-        rows.each(function (index, element) {
-            var line = $(element);
-            var rec = line.data('linkedObj');
-
-            rec.line.ru = line.find('textarea').val();
-            newData.push(rec);
-
-            if (!--count) doMyThing();
         });
     });
 
     $('#save-file').click(function () {
-        var rows = $('.line-row');
-        var count = rows.length;
-        var newData = [];
-
-        function doMyThing() {
+        bigSave(function doMyThing(newData) {
             setData(newData);
 
             var srtRes = getDataJSON();
@@ -264,16 +288,6 @@ $(document).ready(function () {
                     }, dummyFoo);
                 }, dummyFoo
             );
-        }
-
-        rows.each(function (index, element) {
-            var line = $(element);
-            var rec = line.data('linkedObj');
-
-            rec.line.ru = line.find('textarea').val();
-            newData.push(rec);
-
-            if (!--count) doMyThing();
         });
     });
 });
