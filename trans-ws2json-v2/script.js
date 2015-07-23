151b789a45a9dd78607896db116bbf02c9985916
mongolrgata <mongolrgata@gmail.com>
2015-07-24 00:26:01 +0300

    trans

--- trans-ws2json-v2/script.js
+++ trans-ws2json-v2/script.js
@@ -54,6 +54,53 @@ var $nameTemplate = $(
     '</tr>                              '
 );
 
+function getSelectionCoords(win) {
+    win = win || window;
+    var doc = win.document;
+    var sel = doc.selection, range, rects, rect;
+    var x = 0, y = 0;
+    if (sel) {
+        if (sel.type != "Control") {
+            range = sel.createRange();
+            range.collapse(true);
+            x = range.boundingLeft;
+            y = range.boundingTop;
+        }
+    } else if (win.getSelection) {
+        sel = win.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0).cloneRange();
+            if (range.getClientRects) {
+                range.collapse(true);
+                rects = range.getClientRects();
+                if (rects.length > 0) {
+                    rect = rects[0];
+                }
+                x = rect.left;
+                y = rect.top;
+            }
+            // Fall back to inserting a temporary element
+            if (x == 0 && y == 0) {
+                var span = doc.createElement("span");
+                if (span.getClientRects) {
+                    // Ensure span has dimensions and position by
+                    // adding a zero-width space character
+                    span.appendChild( doc.createTextNode("\u200b") );
+                    range.insertNode(span);
+                    rect = span.getClientRects()[0];
+                    x = rect.left;
+                    y = rect.top;
+                    var spanParent = span.parentNode;
+                    spanParent.removeChild(span);
+
+                    // Glue any broken text nodes back together
+                    spanParent.normalize();
+                }
+            }
+        }
+    }
+    return { x: x, y: y };
+}
 function getData() {
     return JSON.parse(localStorage.getItem('f_data_v2') || '[]');
 }
@@ -404,6 +451,40 @@ $(document).ready(function () {
             this.focus();
             this[0].setSelectionRange(this.val().length - 1, this.val().length - 1);
         }).call($text);
+    }
+    if (event.which === 84 && event.altKey) {
+        function getSelectionText() {
+            var text = "";
+            if (window.getSelection) {
+                text = window.getSelection().toString();
+            }
+            return text;
+        }
+
+        var text = getSelectionText();
+        var coor = getSelectionCoords();
+        var bScroll = $($('body')[0]).scrollTop();
+        var x = coor.x;
+        var y = coor.y + bScroll - 22;
+
+        $.get(
+            'https://translate.yandex.net/api/v1.5/tr.json/translate',
+            {
+                key: 'trnsl.1.1.20150723T181540Z.cfda1cc4fccb8bb0.372eb61d5cb01dde22bfc3a3cd8c468fd2dd8e66',
+                text: text,
+                lang: 'en-ru',
+                format: 'html'
+            },
+            function(result) {
+
+                $('<div class="tr-popup"/>').text(result.text.join(' | ')).css({
+                    top: y,
+                    left: x
+                }).click(function() {
+                    this.remove();
+                }).appendTo('body');
+            }
+        );
     }
     if (event.which === '1'.charCodeAt(0) && event.altKey) {
         if (!event.shiftKey) {
