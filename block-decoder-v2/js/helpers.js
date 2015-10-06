1024db3f3da46ba060ce6b45066f204f459c96a7
mongolrgata <mongolrgata@gmail.com>
2015-10-06 19:02:11 +0300

    fix

--- block-decoder-v2/js/helpers.js
+++ block-decoder-v2/js/helpers.js
@@ -3,7 +3,7 @@ define(['extends'], {
         return byte.toString(16).toUpperCase().rJust(2, '0');
     },
     split: function split(string) {
-        return string.match(/(?:\[)..(?:\])/g).map(function (value) {
+        return string.match(/\[..]/g).map(function (value) {
             return parseInt(value.slice(1, 3), 16);
         });
     }
