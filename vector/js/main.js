1d24e3533ff14db21909aadef8c4416db52aa2e1
mongolrgata <mongolrgata@gmail.com>
2015-12-20 20:40:38 +0300

    vector

---
+++ vector/js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by mongolrgata
+ */
+
+require.config({
+    paths: {
+        jquery: 'lib/jquery-2.1.4.min',
+        doT: 'lib/doT.min',
+        BWImage: 'modules/BWImage/BWImage.module'
+    }
+});
+
+require(['jquery', 'doT', 'BWImage'], function ($, doT, BWImage) {
+    $(document).ready(function () {
+        $('button').click(function () {
+            var image = new BWImage();
+            image.loadTest($('body'));
+
+            $(this).hide();
+        });
+    });
+});
