0015755861e8d4eeca32b40a83c2b644fb269a8e
aa.shirkin <aa.shirkin@tensor.ru>
2016-06-14 16:55:21 +0300

    loadtest

--- loadtest/script.js
+++ loadtest/script.js
@@ -2,14 +2,19 @@
  * Created by aa.shirkin on 14.06.2016.
  */
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function () {
    var worker = new Worker('worker.js');
 
    worker.onmessage = function(e) {
       if (e.data.done) {
-         $('div').detach();
+         document.getElementsByTagName('body')[0].innerHTML = 'DONE';
+         return;
       }
       
       document.getElementsByTagName('div')[0].style.width = e.data.percent;
+   };
+
+   worker.onerror = function(e) {
+      console.log(e);
    };
 });
--- loadtest/worker.js
+++ loadtest/worker.js
@@ -2,33 +2,46 @@
  * Created by aa.shirkin on 14.06.2016.
  */
 
-const MAX_I = 5;
-const MAX_J = 2;
-const MAX_K = 3;
+const MAX_I = 100;
+const MAX_J = 100;
+const MAX_K = 1000;
 
-var i = 0, j = 0, k = 0;
-var id = setInterval(function() {
-   self.postMessage({'percent': (Math.round((i * MAX_J * MAX_K + j * MAX_K + k) / (MAX_I * MAX_J * MAX_K) * 100)) + '%', 'done': false});
+// var i = 0, j = 0, k = 0;
+// var id = setInterval(function() {
+//    self.postMessage({'percent': (Math.round((i * MAX_J * MAX_K + j * MAX_K + k) / (MAX_I * MAX_J * MAX_K) * 100)) + '%', 'done': false});
+//
+//    if (i === MAX_I) {
+//       self.postMessage({'done': true});
+//       clearInterval(id);
+//       return;
+//    }
+//
+//    if (j === MAX_J) {
+//       ++i;
+//       j = 0;
+//
+//       return;
+//    }
+//
+//    if (k === MAX_K) {
+//       ++j;
+//       k = 0;
+//       return;
+//    }
+//
+//    // do something
+//    ++k;
+// }, 10);
 
-   if (i === MAX_I) {
-      self.postMessage({'done': true});
-      clearInterval(id);
-      return;
+var percent = 0;
+for (var i = 0; i < MAX_I; ++i) {
+   self.postMessage({'percent': percent});
+   for (var j = 0; j < MAX_J; ++j) {
+      for (var k = 0; k < MAX_K; ++k) {
+         percent = (Math.round((i * MAX_J * MAX_K + j * MAX_K + k) / (MAX_I * MAX_J * MAX_K) * 100)) + '%';
+      }
    }
+}
 
-   if (j === MAX_J) {
-      ++i;
-      j = 0;
-
-      return;
-   }
-
-   if (k === MAX_K) {
-      ++j;
-      k = 0;
-      return;
-   }
-
-   // do something
-   ++k;
-}, 10);
+self.postMessage({'done': true});
+close();
