b10c53b08819190c626c6776b0beb2e07fe76e17
aa.shirkin <aa.shirkin@tensor.ru>
2016-06-23 17:36:41 +0300

    atelier

--- atelier-v2/main.ctl
+++ atelier-v2/main.ctl
@@ -1,4 +1,5 @@
 <div class="menu">
     <button class="save-cauldron">Сохранить</button>
+    <button class="reset-cauldron">Сбросить</button>
 </div>
 <div class="cauldron-box"></div>
--- atelier-v2/script.js
+++ atelier-v2/script.js
@@ -5,20 +5,29 @@
 $(document).ready(function () {
     const STORAGE_KEY = 'f134e149-3e22-433d-8b61-cea2b9eb6f53';
 
-    var cauldronStorage = new GameStorage(STORAGE_KEY, Cauldron);
-    var cauldron = cauldronStorage.getObject();
+    var loading = function() {
+        var cauldronStorage = new GameStorage(STORAGE_KEY, Cauldron);
+        var cauldron = cauldronStorage.getObject();
 
-    var ctl = new CTL('main.ctl');
-    var $template = ctl.applyCfg({
-        'save-cauldron': {
-            click: function () {
-                cauldronStorage.saveObject();
+        var ctl = new CTL('main.ctl');
+        var $template = ctl.applyCfg({
+            'save-cauldron': {
+                click: function () {
+                    cauldronStorage.saveObject();
+                }
+            },
+            'reset-cauldron': {
+                click: function () {
+                    localStorage.clear();
+                    loading();
+                }
             }
-        }
-    });
+        });
 
-    var $body = $('body');
+        var $body = $('body');
+        $body.empty().append($template);
+        cauldron.visualize($body.find('.cauldron-box'));
+    };
 
-    $body.append($template);
-    cauldron.visualize($body.find('.cauldron-box'));
+    loading();
 });
