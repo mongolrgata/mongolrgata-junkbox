063930cb5aa06684e155b76af2c4df27c2356652
mongolrgata <mongolrgata@gmail.com>
2017-05-29 20:34:19 +0300

    B-splines

--- B-spline/js/modules/Filter.js
+++ B-spline/js/modules/Filter.js
@@ -8,9 +8,35 @@ define(['Point', 'BicubicSpline', 'Pixel'], function (Point, BicubicSpline, Pixe
             let startPoint = new Point(0, 0);
             let endPoint = new Point(255, 255);
 
-            this._splineR = new BicubicSpline([startPoint, new Point(before.r, after.r), endPoint]);
-            this._splineG = new BicubicSpline([startPoint, new Point(before.g, after.g), endPoint]);
-            this._splineB = new BicubicSpline([startPoint, new Point(before.b, after.b), endPoint]);
+            if (0 < before.r && before.r < 255) {
+                this._splineR = new BicubicSpline([startPoint, new Point(before.r, after.r), endPoint]);
+            } else {
+                if (before.r === 0) {
+                    this._splineR = new BicubicSpline([new Point(before.r, after.r), endPoint]);
+                } else {
+                    this._splineR = new BicubicSpline([startPoint, new Point(before.r, after.r)]);
+                }
+            }
+
+            if (0 < before.g && before.g < 255) {
+                this._splineG = new BicubicSpline([startPoint, new Point(before.g, after.g), endPoint]);
+            } else {
+                if (before.g === 0) {
+                    this._splineG = new BicubicSpline([new Point(before.g, after.g), endPoint]);
+                } else {
+                    this._splineG = new BicubicSpline([startPoint, new Point(before.g, after.g)]);
+                }
+            }
+
+            if (0 < before.b && before.b < 255) {
+                this._splineB = new BicubicSpline([startPoint, new Point(before.b, after.b), endPoint]);
+            } else {
+                if (before.b === 0) {
+                    this._splineB = new BicubicSpline([new Point(before.b, after.b), endPoint]);
+                } else {
+                    this._splineB = new BicubicSpline([startPoint, new Point(before.b, after.b)]);
+                }
+            }
         }
 
         /**
